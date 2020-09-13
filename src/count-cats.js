74f7dedd9d3d880517156a37eb1597466b5d27c7
Sergei Shipilou <sergeishipilou@yandex.by>
2020-09-13 14:57:02 +0300

    feat: add task solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  
+  let cats = 0;
+
+  backyard.forEach((item) => {
+    item.forEach((item) => {
+      if (item === '^^') {
+        cats += 1;
+      }
+    })
+  });
+    
+  return cats; 
 };
