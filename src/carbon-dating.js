49794aa9e6b9887a303eefcf013e1462ff14ddcb
Sergei Shipilou <sergeishipilou@yandex.by>
2020-09-15 21:08:26 +0300

    feat: add solution to task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,32 +6,17 @@ const RATE_CONSTANT = 0.693;
 
 module.exports = function dateSample(sampleActivity) {
 
-    const MODERN_ACTIVITY = 15; 
-    const HALF_LIFE_PERIOD = 5730;
-    const RATE_CONSTANT = 0.693;
+    const k = RATE_CONSTANT / HALF_LIFE_PERIOD;
+    const SAMPLE_ACTIVITY_NUMBER = parseFloat(sampleActivity, 8);
 
-    let k = RATE_CONSTANT / HALF_LIFE_PERIOD;
+    if (!sampleActivity
+      || typeof sampleActivity !== 'string'
+      || !isFinite(SAMPLE_ACTIVITY_NUMBER)
+      || (SAMPLE_ACTIVITY_NUMBER >= MODERN_ACTIVITY)
+      || (SAMPLE_ACTIVITY_NUMBER <= 0)
 
-    if (typeof sampleActivity !== 'string') {
+    ) return false;
 
-      return false; 
-
-    } else if (sampleActivity.length === 0) {
-
-      return false;
-
-    } else if (Number(sampleActivity) < 0) {
-
-      return decay = -1 * Math.ceil( (Math.log(MODERN_ACTIVITY / (Math.abs(Number(sampleActivity))))) / k );;
-
-    } else if (sampleActivity.match(/[^0-9]/)) {
-
-      return false;
-
-    } else {
-
-      return decay = Math.ceil( (Math.log(MODERN_ACTIVITY / (Number(sampleActivity)))) / k );
-
-    }
+    return Math.ceil(Math.log(MODERN_ACTIVITY / SAMPLE_ACTIVITY_NUMBER) / k );
 
 };
