c296f72330bb0d100a23e1185984dadec797cf18
Sergei Shipilov <sergeishipilou@yandex.by>
2020-10-27 21:48:03 +0300

    feat: add task solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,32 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator}) {
+module.exports = function repeater(str, {repeatTimes, separator = '+', addition, additionRepeatTimes, additionSeparator = '|'}) {
   let arr = [];
   
-  if (additionRepeatTimes === undefined) {
-    constadditionSeparator = "|";
-    arr = new Array(1).fill(addition);
-  } else {
-    arr = new Array(additionRepeatTimes).fill(addition);
-  }
-  
+  arr = new Array(additionRepeatTimes).fill(addition);
+
   let addSeparator = arr.map((item, index) => {
-    return `${index ? `${additionSeparator}` : ''}${item}`;
+    return `${index ? `${additionSeparator || this.additionSeparator}` : ''}${item}`;
   }).join('');
   
-	let result = str + addSeparator;
+  if (addSeparator === 'undefined') {
+    result = str
+  } else result = str + addSeparator;
   
-  if (repeatTimes === undefined) {
-    separator = '+';
-    lastArr = new Array(1).fill(result)
-  } else {
-    lastArr = new Array(repeatTimes).fill(result)
-  }    
-
-  lastSeparator = lastArr.map((item, index) => {
-    return `${index ? `${separator}` : ''}${item}`;
+  let lastArr = new Array(repeatTimes).fill(result)  
+  
+  let lastSeparator = lastArr.map((item, index) => {
+    return `${index ? `${separator || this.separator}` : ''}${item}`;
   }).join('');
   
   return lastSeparator;
 };
-  
